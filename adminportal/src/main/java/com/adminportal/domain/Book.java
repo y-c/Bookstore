962f599c226504183956d33d61974226be30fb0c
Yu Chen <yuchen.wayne@gmail.com>
2017-05-24 12:32:56 -0400

    27

--- adminportal/src/main/java/com/adminportal/domain/Book.java
+++ adminportal/src/main/java/com/adminportal/domain/Book.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,6 +31,7 @@ public class Book {
 	private String description;
 	private int inStockNumber;
 	
+	@Transient
 	private MultipartFile bookImage;
 
 	public Long getId() {
