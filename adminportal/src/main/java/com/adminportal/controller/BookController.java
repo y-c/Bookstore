f3835adb361b23bb7fe693a80ec6a60a3773cc4e
Yu Chen <yuchen.wayne@gmail.com>
2017-05-30 03:02:03 -0400

    39

--- adminportal/src/main/java/com/adminportal/controller/BookController.java
+++ adminportal/src/main/java/com/adminportal/controller/BookController.java
@@ -3,6 +3,8 @@ package com.adminportal.controller;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,32 +28,31 @@ public class BookController {
 	@Autowired
 	private BookService bookService;
 
-	@RequestMapping("/add")
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
 	public String addBook(Model model) {
 		Book book = new Book();
 		model.addAttribute("book", book);
 		return "addBook";
 	}
 
-	@RequestMapping(value="/add", method=RequestMethod.POST)
-	public String addBookPost(
-			@ModelAttribute("book") Book book, HttpServletRequest request
-			) {
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String addBookPost(@ModelAttribute("book") Book book, HttpServletRequest request) {
 		bookService.save(book);
-		
+
 		MultipartFile bookImage = book.getBookImage();
-		
+
 		try {
 			byte[] bytes = bookImage.getBytes();
-			String name = book.getId()+".png";
-			BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File("src/main/resources/static/image/book/"+name)));
+			String name = book.getId() + ".png";
+			BufferedOutputStream stream = new BufferedOutputStream(
+					new FileOutputStream(new File("src/main/resources/static/image/book/" + name)));
 			stream.write(bytes);
 			stream.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		return "redirect:booklist";
+
+		return "redirect:bookList";
 	}
 	
 	@RequestMapping("/bookInfo")
@@ -70,10 +71,37 @@ public class BookController {
 		return "updateBook";
 	}
 	
+	@RequestMapping(value="/updateBook", method=RequestMethod.POST)
+	public String updateBookPost(@ModelAttribute("book") Book book, HttpServletRequest request) {
+		bookService.save(book);
+		
+		MultipartFile bookImage = book.getBookImage();
+		
+		if(!bookImage.isEmpty()) {
+			try {
+				byte[] bytes = bookImage.getBytes();
+				String name = book.getId() + ".png";
+				
+				Files.delete(Paths.get("src/main/resources/static/image/book/"+name));
+				
+				BufferedOutputStream stream = new BufferedOutputStream(
+						new FileOutputStream(new File("src/main/resources/static/image/book/" + name)));
+				stream.write(bytes);
+				stream.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return "redirect:/book/bookInfo?id="+book.getId();
+	}
+	
 	@RequestMapping("/bookList")
 	public String bookList(Model model) {
 		List<Book> bookList = bookService.findAll();
-		model.addAttribute("bookList", bookList);
+		model.addAttribute("bookList", bookList);		
 		return "bookList";
+		
 	}
+
 }
